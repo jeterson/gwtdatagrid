29d71a6b04e9588c8ce75d60a5d028122d169614
jeterson <jeterson@TI-05.cairunet.ad.br>
2017-09-26 08:05:35 -0400

    .

--- gwtdatagrid/src/com/jeterson/gwtdatagrid/client/Gwtdatagrid.java
+++ gwtdatagrid/src/com/jeterson/gwtdatagrid/client/Gwtdatagrid.java
@@ -1,8 +1,6 @@
 package com.jeterson.gwtdatagrid.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -27,9 +25,6 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class Gwtdatagrid implements EntryPoint {
 	
 	public void onModuleLoad() {
-		FlowPanel  f = new FlowPanel();
-		f.setHeight("300px");
-		f.add(new DataGridView());
-		RootPanel.get().add(f);
+		//TODO
 	}
 }
